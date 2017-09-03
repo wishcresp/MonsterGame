dad5619744cc1414a0ebc8545ab722d68871c34c
Michael Dao <mifrent@hotmail.com>
2017-09-03 22:48:01 +1000

    JUnit Update to exception class

--- server/MonsterGameExceptionTest.java
+++ server/MonsterGameExceptionTest.java
@@ -1,16 +1,13 @@
 import static org.junit.Assert.*;
-import java.util.ArrayList;
 import org.junit.Test;
 
 //This is the JUnit test, this will test whether it works when a custom exception is thrown 
 //Michael Dao s3668300 23/08/17
 
-public class MonsterGameExceptionTest extends Exception {
+public class MonsterGameExceptionTest {
 
 	@Test(expected = MonsterGameException.class)
-	public void testThrows() throws MonsterGameException {
+	public void test() throws MonsterGameException {
 		throw new MonsterGameException("There has been an error");
 	}
-
 }
-
